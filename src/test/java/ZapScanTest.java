ef27587134786a40c719e2a81b2af2aed815ecc0
kumarvastav <kumar.vastav@gmail.com>
2016-12-16 18:34:57 +0530

    First Commit: Basic security regression with ZAP Client API's

---
+++ src/test/java/ZapScanTest.java
@@ -0,0 +1,243 @@
+/**
+ * Created by dharmens on 12/14/16.
+ */
+
+import net.continuumsecurity.proxy.ScanningProxy;
+import net.continuumsecurity.proxy.Spider;
+import net.continuumsecurity.proxy.ZAProxyScanner;
+//import net.continuumsecurity.web.drivers.DriverFactory;
+import org.apache.log4j.Logger;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.Proxy;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.zaproxy.clientapi.core.Alert;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class ZapScanTest {
+
+    static Logger log = Logger.getLogger(ZapScanTest.class.getName());
+    private final static String ZAP_PROXYHOST = "localhost";
+    private final static int ZAP_PROXYPORT = 8888;
+    private final static String ZAP_APIKEY = "87tdnb0qek3d6o9sfmrg39nqjk";
+
+    // Change this to the appropriate driver for the OS, alternatives in the drivers directory
+    //private final static String CHROME_DRIVER_PATH = "drivers/chromedriver-mac";
+    private final static String CHROME_DRIVER_PATH = "/usr/local/bin/chromedriver";
+    private final static String MEDIUM = "MEDIUM";
+    private final static String HIGH = "HIGH";
+    private ScanningProxy zapScanner;
+    private Spider zapSpider;
+    private WebDriver driver;
+    //private MyAppNavigation myApp;
+    private final static String[] policyNames = {"directory-browsing","cross-site-scripting","sql-injection","path-traversal","remote-file-inclusion","server-side-include",
+            "script-active-scan-rules","server-side-code-injection","external-redirect","crlf-injection"};
+    int currentScanID;
+
+
+    @Before
+    public void setup() {
+        zapScanner = new ZAProxyScanner(ZAP_PROXYHOST,ZAP_PROXYPORT,ZAP_APIKEY);
+        zapScanner.clear(); //Start a new session
+        zapSpider = (Spider)zapScanner;
+        log.info("Created client to ZAP API");
+        Proxy proxy = new Proxy();
+        proxy.setHttpProxy(ZAP_PROXYHOST + ":" + ZAP_PROXYPORT);
+        proxy.setSslProxy(ZAP_PROXYHOST + ":" + ZAP_PROXYPORT);
+        //driver = DriverFactory.createProxyDriver("chrome",createZapProxyConfigurationForWebDriver(), CHROME_DRIVER_PATH);
+        driver = createChromeDriver(createProxyCapabilities(proxy),CHROME_DRIVER_PATH);
+        //myApp = new MyAppNavigation(driver);
+        //myApp.registerUser(); //Doesn't matter if user already exists, bodgeit just throws an error
+    }
+
+    @After
+    public void after() {
+        driver.quit();
+    }
+
+    @Test
+    public void scanWithoutLogin(){
+        driver.get("https://gauge.mybahmni.org");
+        System.out.println("Spidering...");
+        spiderWithZap();
+        System.out.println("Spider done.");
+
+        setAlertAndAttackStrength();
+        zapScanner.setEnablePassiveScan(true);
+        scanWithZap();
+
+        List<Alert> alerts = filterAlerts(zapScanner.getAlerts());
+        logAlerts(alerts);
+        assertThat(alerts.size(), equalTo(0));
+    }
+
+    private void logAlerts(List<Alert> alerts) {
+        for (Alert alert : alerts) {
+            System.out.println("Alert: "+alert.getAlert()+" at URL: "+alert.getUrl()+" Parameter: "+alert.getParam()+" CWE ID: "+alert.getCweId());
+        }
+    }
+    /*
+        Remove false positives, filter based on risk and reliability
+     */
+    private List<Alert> filterAlerts(List<Alert> alerts) {
+        List<Alert> filtered = new ArrayList<Alert>();
+        for (Alert alert : alerts) {
+            //if (alert.getRisk().equals(Alert.Risk.High) && alert.getConfidence() != Alert.Confidence.Low) filtered.add(alert);
+            if (alert.getRisk().equals(Alert.Risk.High) && alert.getRisk().equals(Alert.Risk.Medium)) filtered.add(alert);
+        }
+        return filtered;
+    }
+
+    public void setAlertAndAttackStrength() {
+        for (String policyName : policyNames) {
+            String ids = enableZapPolicy(policyName);
+            for (String id : ids.split(",")) {
+                zapScanner.setScannerAlertThreshold(id,MEDIUM);
+                zapScanner.setScannerAttackStrength(id,HIGH);
+            }
+        }
+    }
+
+    private void scanWithZap() {
+        System.out.println("Scanning...");
+        zapScanner.scan("https://gauge.mybahmni.org");
+        currentScanID = zapScanner.getLastScannerScanId();
+        int complete = 0;
+        while (complete < 100) {
+            complete = zapScanner.getScanProgress(currentScanID);
+            System.out.println("Scan is " + complete + "% complete.");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Scanning done.");
+    }
+
+    private String enableZapPolicy(String policyName) {
+        String scannerIds = null;
+        switch (policyName.toLowerCase()) {
+            case "directory-browsing":
+                scannerIds = "0";
+                break;
+            case "cross-site-scripting":
+                scannerIds = "40012,40014,40016,40017";
+                break;
+            case "sql-injection":
+                scannerIds = "40018";
+                break;
+            case "path-traversal":
+                scannerIds = "6";
+                break;
+            case "remote-file-inclusion":
+                scannerIds = "7";
+                break;
+            case "server-side-include":
+                scannerIds = "40009";
+                break;
+            case "script-active-scan-rules":
+                scannerIds = "50000";
+                break;
+            case "server-side-code-injection":
+                scannerIds = "90019";
+                break;
+            case "remote-os-command-injection":
+                scannerIds = "90020";
+                break;
+            case "external-redirect":
+                scannerIds = "20019";
+                break;
+            case "crlf-injection":
+                scannerIds = "40003";
+                break;
+            case "source-code-disclosure":
+                scannerIds = "42,10045,20017";
+                break;
+            case "shell-shock":
+                scannerIds = "10048";
+                break;
+            case "remote-code-execution":
+                scannerIds = "20018";
+                break;
+            case "ldap-injection":
+                scannerIds = "40015";
+                break;
+            case "xpath-injection":
+                scannerIds = "90021";
+                break;
+            case "xml-external-entity":
+                scannerIds = "90023";
+                break;
+            case "padding-oracle":
+                scannerIds = "90024";
+                break;
+            case "el-injection":
+                scannerIds = "90025";
+                break;
+            case "insecure-http-methods":
+                scannerIds = "90028";
+                break;
+            case "parameter-pollution":
+                scannerIds = "20014";
+                break;
+            default : throw new RuntimeException("No policy found for: "+policyName);
+        }
+        if (scannerIds == null) throw new RuntimeException("No matching policy found for: " + policyName);
+        zapScanner.setEnableScanners(scannerIds, true);
+        return scannerIds;
+    }
+
+    private static Proxy createZapProxyConfigurationForWebDriver() {
+        Proxy proxy = new Proxy();
+        proxy.setHttpProxy(ZAP_PROXYHOST + ":" + ZAP_PROXYPORT);
+        proxy.setSslProxy(ZAP_PROXYHOST + ":" + ZAP_PROXYPORT);
+        return proxy;
+    }
+
+    private void spiderWithZap() {
+        zapSpider.excludeFromSpider("https://gauge.mybahmni.org/bahmni/home/index.html#/login");
+        zapSpider.setThreadCount(5);
+        zapSpider.setMaxDepth(5);
+        zapSpider.setPostForms(false);
+        zapSpider.spider("https://gauge.mybahmni.org");
+        int spiderID = zapSpider.getLastSpiderScanId();
+        int complete  = 0;
+        while (complete < 100) {
+            complete = zapSpider.getSpiderProgress(spiderID);
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        for (String url : zapSpider.getSpiderResults(spiderID)) {
+            System.out.println("Found URL: "+url);
+        }
+    }
+
+    public static WebDriver createChromeDriver(DesiredCapabilities capabilities, String path) {
+        System.setProperty("webdriver.chrome.driver", path);
+        if (capabilities != null) {
+            capabilities.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+            return new ChromeDriver(capabilities);
+        } else return new ChromeDriver();
+
+    }
+
+    public static DesiredCapabilities createProxyCapabilities(Proxy proxy) {
+        DesiredCapabilities capabilities = DesiredCapabilities.chrome();
+        capabilities.setCapability("proxy", proxy);
+        return capabilities;
+    }
+
+}
